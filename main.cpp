e8d3f29a46c35ef56ba8522be6fc64cf73849c73
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-06-02 21:58:59 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+//ДЗ #1
+//int main()
+//{
+   // setlocale(0, "RUS");
+   // int x1;
+   // int y1;
+    
+   // int x2;
+    //int y2;
+
+    //cout << "Введите координаты первой точки прямой(X,Y):\n";
+    //cin >> x1;
+    //cin >> y1;
+
+    //cout << "Введите координаты второй точки прямой(X,Y):\n";
+    //cin >> x2;
+    //cin >> y2;
+
+    //if (x1 == x2) 
+    //{
+    //    cout << "Прямая параллельна оси ординат";
+    //}
+    //else if (y1 == y2) 
+    //{
+     //   cout << "Прямая параллельна оси абсцисс";
+    //}
+    //else 
+    //{
+    //    cout << "Прямая не параллельна оси ординат или абсцисс";
+    //}
+
+//}
+
+
+
+
+
+
+//ДЗ #2
+//int main()
+//{
+  //  setlocale(0, "RUS");
+   // cout << "Введите число:";
+   // double num;
+   // cin >> num;
+
+    //if (num == int(num))
+    //{
+     //   cout << "Число не имеет вещественной части";
+    //}
+    //else if (num != int(num)) 
+    //{
+    //    cout << "Число имеет вещественную часть";
+    //}
+//}
+
+
+
+
+
+
+
+//ДЗ #3
+//int main()
+//{  
+    //setlocale(0, "RUS");
+
+    //int Hr;
+    //int Min;
+    //int Sec;
+
+    //cout << "Введите текущее время(час,минута,секунда):\n";
+    //cin >> Hr;
+    //cin >> Min;
+    //cin >> Sec;
+
+    //if (Hr <= 23 && Hr >= 0 && Min <= 60 && Min >= 0 && Sec <= 60 && Sec >= 0) 
+    //{
+     //   cout << "Время указано корректно";
+    //}
+    //else 
+    //{
+    //    cout << "Время указано некоректно";
+    //}
+//}
+
+
+  
+
+
+
+
+
+
+//ДЗ #4
+//int main() 
+//{
+    //setlocale(0, "RUS");
+    //cout << "Введите номер билета(6 цифр):\n";
+    //int number;
+    //cin >> number;
+
+    //int hundred_thousands = number / 10000 % 1000 / 10;
+    //int ten_thousands =  number / 1000 % 100 / 10;
+    //int thousands =  number / 1000 % 10;
+    //int hundreds = number % 1000 / 100;
+    //int dozens = number % 100 / 10;
+    //int units = number % 10;
+
+    //if (number >= 000000 && number <= 999999) 
+    //{
+       // int sum_first = hundred_thousands + ten_thousands + thousands;
+       // int sum_second = hundreds + dozens + units;
+        
+        //if (sum_first == sum_second)
+        //{
+        //    cout << "Ваш билет счастливый!";
+        //}
+        //else 
+        //{
+        //    cout << "Ваш билет не счастливый!";
+        //}
+    //}
+    //else 
+    //{
+    //    cout << "Ошибка";
+    //}
+//}
+
+
+
+
+  
+  
+  
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
